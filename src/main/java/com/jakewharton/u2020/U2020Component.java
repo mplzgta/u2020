1a52ca40748f6458254af4ad83c8d3a39162f162
DPozega <pozegadi@gmail.com>
2015-08-31 17:20:22 -0400

    sloppy dagger 2 implementation for productionRelease

---
+++ src/main/java/com/jakewharton/u2020/U2020Component.java
@@ -0,0 +1,7 @@
+package com.jakewharton.u2020;
+
+/**
+ * Created by dpozega on 8/31/15.
+ */
+public class U2020Component {
+}
---
+++ src/main/java/com/jakewharton/u2020/data/DataComponent.java
@@ -0,0 +1,7 @@
+package com.jakewharton.u2020.data;
+
+/**
+ * Created by dpozega on 8/31/15.
+ */
+public interface DataComponent {
+}
